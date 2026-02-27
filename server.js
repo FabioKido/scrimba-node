b3fe1b320e4d59f6e08fd2537e2a65abb56c01eb
FabioKido <fabiohenryquemesquita@gmail.com>
2026-02-27 20:47:20 -0300

    methods res.write and res.end

--- server.js
+++ server.js
@@ -3,7 +3,13 @@ import http from 'node:http';
 const PORT = 8000;
 
 const server = http.createServer((req, res) => {
-    res.end("Hello from server!");
+    res.write("Some data... \n");      // 'res.write' precisa (obrigatório) que o método 'res.end' seja implementado no final
+    
+    res.write("More data... \n");
+    
+    res.end("Hello from server!", () => {
+        console.log("Response closed!");
+    });  // Aceita até três params: Dado, Codificação (ex: utf-8) e uma Função de Callback
 });
 
 server.listen(PORT, () => {
