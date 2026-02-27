7219e98f25327f599afea06bb5bd9bb4c50f1e3d
FabioKido <fabiohenryquemesquita@gmail.com>
2026-02-27 20:35:30 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,11 @@
+import http from 'node:http';
+
+const PORT = 8000;
+
+const server = http.createServer((req, res) => {
+    res.end("Hello from server!");
+});
+
+server.listen(PORT, () => {
+    console.log(`Server running: http://localhost:${PORT}`);
+});
