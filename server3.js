dc9cb39b5c39b9b384857de3d314ef1c309c4ae3
FabioKido <fabiohenryquemesquita@gmail.com>
2026-02-28 20:26:33 -0300

    method res.setHeader & property res.statusCode

--- server3.js
+++ server3.js
@@ -1,4 +1,4 @@
-import http, { get } from 'node:http';
+import http from 'node:http';
 import { getDataFromDB } from './database/db.js';
 
 const PORT = 8000;
@@ -7,6 +7,8 @@ const server = http.createServer(async (req, res) => {
     const destinations = await getDataFromDB();
 
     if(req.url === '/api' && req.method === 'GET') {
+        res.setHeader('Content-Type', 'application/json');
+        res.statusCode = 200;
         res.end(JSON.stringify(destinations));
     }
 });
